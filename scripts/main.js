9975ce42962d4561997577603c7d67f83be68c30
Brandon <railsdev14@gmail.com>
2016-02-10 10:40:54 -0500

    Added nested components

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,70 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-// StorePicker
-// This will let us make <StorePicker/>
-var StorePicker = React.createClass({
-  render : function() {
+
+/*
+  App
+*/
+var App = React.createClass({
+  render: function() {
     return (
-      <p>hello</p>
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header/>
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
     )
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+/*
+  Header
+*/
+var Header = React.createClass({
+  render: function() {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+/*
+  Order
+*/
+var Order = React.createClass({
+  render: function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+  Inventory
+*/
+var Inventory = React.createClass({
+  render: function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+/*
+  StorePicker
+  This will let us make <StorePicker/>
+*/
+var StorePicker = React.createClass({
+  render : function() {
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" required/>
+        <input type="Submit"/>
+      </form>
+    )
+  }
+});
+
+ReactDOM.render(<App/>, document.querySelector('#main'));
