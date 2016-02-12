36857c8cf2a743924a993a1ca6d0a58addca463e
Brandon <railsdev14@gmail.com>
2016-02-12 14:44:44 -0500

    Added local storage

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,17 @@ var App = React.createClass({
       context: this,
       state: 'fishes'
     });
+
+    var localStoreageRef = localStorage.getItem('order-' + this.props.params.storeId);
+
+    if(localStoreageRef) {
+      this.setState({
+        order: JSON.parse(localStoreageRef)
+      });
+    }
+  },
+  componentWillUpdate: function(nextProps, nextState) {
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
   },
   addToOrder: function(key) {
     this.state.order[key] = this.state.order[key] + 1 || 1;
@@ -163,7 +174,7 @@ var Order = React.createClass({
       return <li key={key}>Sorry, fish is no longer available.</li>
     }
     return (
-      <li>
+      <li key={key}>
       {count}lbs
       {fish.name}
       <span className="price">{h.formatPrice(count * fish.price)}</span>
